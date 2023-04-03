ca8bcbc80b3eacdee8a5bd4f8794b073fb25476f
burasate <61697687+burasate@users.noreply.github.com>
2023-04-03 20:46:30 +0700

    Update install.py

--- service/install.py
+++ service/install.py
@@ -8,7 +8,12 @@ import os, json, sys
 maya_app_dir = mel.eval('getenv MAYA_APP_DIR')
 scripts_dir = os.path.abspath(mayaAppDir + os.sep + 'scripts')
 tool_dir = os.path.abspath(scripts_dir + os.sep + 'BRSQuickMotion')
+install_path = os.path.abspath(tool_dir + os.sep + 'Install.mel')
 print(tool_dir, os.path.exists(tool_dir))
+print(install_path, os.path.exists(install_path))
+
+if not os.path.exists(tool_dir) or not os.path.exists(install_path):
+    raise Warning('Install.mel  not found in {}'.format(tool_dir))
 
 """
 from maya import cmds
